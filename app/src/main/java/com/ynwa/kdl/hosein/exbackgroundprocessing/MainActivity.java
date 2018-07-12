fea5127dee15d296a9f2142842c5d868ebaeeab6
hosein kordloo <hosein.kordloo@gmail.com>
2018-07-13 03:00:31 +0430

    The first commit of project

---
+++ app/src/main/java/com/ynwa/kdl/hosein/exbackgroundprocessing/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ynwa.kdl.hosein.exbackgroundprocessing;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "ttt";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // hello there . this is a new project
+        //for testing git
+
+
+    }
+
+
+}
